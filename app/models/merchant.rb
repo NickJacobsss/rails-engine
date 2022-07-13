63fa245a1d687bf88d85a74beb8ca889c82f594a
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-13 10:44:52 -0500

    Feat: find merchant method created

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,11 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
-
   has_many :items
+
+
+  def self.find_merchant(query)
+    where('LOWER(name) like ?', "%#{query.downcase}%")
+    .order("name")
+    .first
+  end
 end
